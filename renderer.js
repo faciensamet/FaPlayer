2c97bb1d4377f6b056d23ef5ed9b8d9481459713
faciensamet <82987097+faciensamet@users.noreply.github.com>
2021-06-02 11:37:05 +0900

    support multi language

--- renderer.js
+++ renderer.js
@@ -40,11 +40,3 @@ document.addEventListener('dragover', (e) => {
     e.preventDefault();
     e.stopPropagation();
 });
-
-document.addEventListener('dragenter', (event) => {
-    console.log('File is in the Drop Space');
-});
-
-document.addEventListener('dragleave', (event) => {
-    console.log('File has left the Drop Space');
-});
