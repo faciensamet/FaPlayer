67bc3d0390f62452c0f090fd058013541055a9b3
faciensamet <82987097+faciensamet@users.noreply.github.com>
2021-06-14 15:47:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-//for update
-require('update-electron-app')({
-  repo: 'faciensamet/FaPlayer',
-  updateInterval: '1 hour',
-  notifyUser: false
-})
+const isDev = require('electron-is-dev');
+
+if (isDev) {
+  console.log('Running in development');
+} else {
+  console.log('Running in production');
+
+  //for update
+  require('update-electron-app')({
+    repo: 'faciensamet/FaPlayer',
+    updateInterval: '1 hour',
+    notifyUser: false
+  })
+
+}
 
 
 const { app, BrowserWindow, ipcMain, Menu, MenuItem, remote } = require('electron')
 const path = require('path')
 const contextMenu = require('electron-context-menu');
-const isDev = require('electron-is-dev');
 
-if (isDev) {
-	console.log('Running in development');
-} else {
-	console.log('Running in production');
-}
 
 console.log('argv : ' + process.argv);
 
@@ -52,21 +55,3 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
-
-contextMenu({
-	prepend: (defaultActions, parameters, browserWindow) => [
-		{
-			label: 'Rainbow',
-			// Only show it when right-clicking images
-			visible: parameters.mediaType === 'image'
-		},
-		{
-			label: 'Search Google for “{selection}”',
-			// Only show it when right-clicking text
-			visible: parameters.selectionText.trim().length > 0,
-			click: () => {
-				shell.openExternal(`https://google.com/search?q=${encodeURIComponent(parameters.selectionText)}`);
-			}
-		}
-	]
-});
